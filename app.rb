75b5743282cffd0e8f610584fa328152cd487c72
Christopher Tompkins <chtompkins026@gmail.com>
2018-12-05 19:53:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		erb :index
 	end
-end
+
+  
+  get "/info" do
+    erb :info 
+  end
+
+
+end #end of the App class
