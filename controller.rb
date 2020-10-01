43b474a41cc109a7b2e064bc775d7668b06250cc
Robert Clarke <r.jason.clarke@gmail.com>
2020-10-01 00:24:37 +0000

    Done.

---
+++ controller.rb
@@ -0,0 +1,15 @@
+get '/' do
+    erb :index
+  end
+
+  get '/' do
+        erb :index
+      end
+
+      get "/info" do
+        "Testing the info page"
+      end
+
+      get '/' do
+    erb :index
+  end
